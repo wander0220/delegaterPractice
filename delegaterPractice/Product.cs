9dc034d269c52477a2cab41c3ee2c59bfb48c586
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-09-25 11:17:24 +0900

    초기 커밋.

---
+++ delegaterPractice/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegaterPractice
+{
+    class Product
+    {
+        private string name;
+        private int price;
+
+        public string Name { get => name; set => name = value; }
+        public int Price { get => price; set => price = value; }
+        
+    }
+}
