f4643dd2a67e95a17c4bc5d3412830ded63fe753
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-09-25 11:30:26 +0900

    mProductList 값 add

--- delegaterPractice/Form1.Designer.cs
+++ delegaterPractice/Form1.Designer.cs
@@ -28,33 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBoxProducts = new System.Windows.Forms.ListBox();
+            this.btnSort1 = new System.Windows.Forms.Button();
+            this.btnSort2 = new System.Windows.Forms.Button();
+            this.btnSort3 = new System.Windows.Forms.Button();
+            this.btnSortReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // listBoxProducts
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 18;
-            this.listBox1.Location = new System.Drawing.Point(13, 13);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(220, 328);
-            this.listBox1.TabIndex = 0;
+            this.listBoxProducts.FormattingEnabled = true;
+            this.listBoxProducts.ItemHeight = 18;
+            this.listBoxProducts.Location = new System.Drawing.Point(13, 13);
+            this.listBoxProducts.Name = "listBoxProducts";
+            this.listBoxProducts.Size = new System.Drawing.Size(220, 328);
+            this.listBoxProducts.TabIndex = 0;
+            // 
+            // btnSort1
+            // 
+            this.btnSort1.Location = new System.Drawing.Point(13, 348);
+            this.btnSort1.Name = "btnSort1";
+            this.btnSort1.Size = new System.Drawing.Size(220, 33);
+            this.btnSort1.TabIndex = 1;
+            this.btnSort1.Text = "정렬(함수이름)";
+            this.btnSort1.UseVisualStyleBackColor = true;
+            // 
+            // btnSort2
+            // 
+            this.btnSort2.Location = new System.Drawing.Point(12, 387);
+            this.btnSort2.Name = "btnSort2";
+            this.btnSort2.Size = new System.Drawing.Size(220, 33);
+            this.btnSort2.TabIndex = 2;
+            this.btnSort2.Text = "정렬(익명 델리게이터)";
+            this.btnSort2.UseVisualStyleBackColor = true;
+            // 
+            // btnSort3
+            // 
+            this.btnSort3.Location = new System.Drawing.Point(13, 426);
+            this.btnSort3.Name = "btnSort3";
+            this.btnSort3.Size = new System.Drawing.Size(220, 33);
+            this.btnSort3.TabIndex = 3;
+            this.btnSort3.Text = "정렬(람다)";
+            this.btnSort3.UseVisualStyleBackColor = true;
+            // 
+            // btnSortReset
+            // 
+            this.btnSortReset.Location = new System.Drawing.Point(13, 465);
+            this.btnSortReset.Name = "btnSortReset";
+            this.btnSortReset.Size = new System.Drawing.Size(220, 33);
+            this.btnSortReset.TabIndex = 4;
+            this.btnSortReset.Text = "정렬 초기화";
+            this.btnSortReset.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(419, 450);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(419, 562);
+            this.Controls.Add(this.btnSortReset);
+            this.Controls.Add(this.btnSort3);
+            this.Controls.Add(this.btnSort2);
+            this.Controls.Add(this.btnSort1);
+            this.Controls.Add(this.listBoxProducts);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBoxProducts;
+        private System.Windows.Forms.Button btnSort1;
+        private System.Windows.Forms.Button btnSort2;
+        private System.Windows.Forms.Button btnSort3;
+        private System.Windows.Forms.Button btnSortReset;
     }
 }
 
--- delegaterPractice/Form1.cs
+++ delegaterPractice/Form1.cs
@@ -12,9 +12,30 @@ namespace delegaterPractice
 {
     public partial class Form1 : Form
     {
+        List<Product> mProductList = new List<Product>();
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetProductList();
+        }
+
+        private void resetProductList()
+        {
+            mProductList.Clear();
+            mProductList.Add(new Product() { Name = "감자", Price = 500 });
+            mProductList.Add(new Product() { Name = "사과", Price = 700 });
+            mProductList.Add(new Product() { Name = "고구마", Price = 400 });
+            mProductList.Add(new Product() { Name = "배추", Price = 600 });
+            mProductList.Add(new Product() { Name = "상추", Price = 300 });
+
+
         }
     }
 }
